68012d97884a4ea941825f62f4593d974a444c60
Ashish_DevOps_learner <100029148+ashishdevops123@users.noreply.github.com>
2024-07-08 20:28:03 +0530

    Update answers.tf

--- answers.tf
+++ answers.tf
@@ -1,1 +1,33 @@
 
+storageClass:
+  name: dynamic-storage
+  isDefault: "true"
+  reclaimPolicy: Delete
+  volumeBindingMode: Immediate
+
+  # Select the provisioner based on your cloud provider
+  # AWS EBS: kubernetes.io/aws-ebs
+  # GCP PD: kubernetes.io/gce-pd
+  provisioner: kubernetes.io/aws-ebs
+  # provisioner: kubernetes.io/gce-pd
+
+
+
+apiVersion: storage.k8s.io/v1
+kind: StorageClass
+metadata:
+  name: {{ .Values.storageClass.name }}-{{ .Release.Namespace }}
+  annotations:
+    storageclass.kubernetes.io/is-default-class: "{{ .Values.storageClass.isDefault }}"
+provisioner: {{ .Values.storageClass.provisioner }}
+parameters:
+  {{- if eq .Values.storageClass.provisioner "kubernetes.io/aws-ebs" }}
+  type: gp2
+  fsType: ext4
+  {{- end }}
+  {{- if eq .Values.storageClass.provisioner "kubernetes.io/gce-pd" }}
+  type: pd-ssd
+  replication-type: none
+  {{- end }}
+reclaimPolicy: {{ .Values.storageClass.reclaimPolicy }}
+volumeBindingMode: {{ .Values.storageClass.volumeBindingMode }}
